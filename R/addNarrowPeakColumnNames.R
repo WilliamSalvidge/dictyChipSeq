b2a51f2ed438cb00dff50201bc51b7c21af04721
William Salvidge <williamsalvidge@Williams-MacBook-Air.local>
2021-07-26 12:15:15 +0100

    Added roxygen documentation for add_narrowPeak_columnNames function. Plus called document() to create the help page for this function. Then updated itt and called document() again.

--- R/addNarrowPeakColumnNames.R
+++ R/addNarrowPeakColumnNames.R
@@ -1,5 +1,14 @@
 
 
+#' add narrowPeak file column names to a GRanges object
+#'
+#' The chipSeeker readPeakFile function creates a GRanges object but the narrowPeak column names are missing.
+#' This function adds them in
+#'
+#' @param x Granges object
+#'
+#' @return Granges object
+#' @export
 add_narrowPeak_columnNames = function (x) {
   # Set metadata column names to reflect narrowPeaks format
   # see http://genome.ucsc.edu/FAQ/FAQformat.html#format12
@@ -8,3 +17,4 @@ add_narrowPeak_columnNames = function (x) {
   return (x)
 
 }
+
