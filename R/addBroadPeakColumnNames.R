9cc59e0a4b546ec2f8247cfaa87f109b711a7140
William Salvidge <williamsalvidge@Williams-MacBook-Air.local>
2021-07-26 12:31:01 +0100

    added add_broadPeak_columnNames function and added documentation

--- R/addBroadPeakColumnNames.R
+++ R/addBroadPeakColumnNames.R
@@ -1,20 +1,19 @@
 
 
-#' add narrowPeak file column names to a GRanges object
+#' add broadPeak file column names to a GRanges object
 #'
 #' The chipSeeker readPeakFile function creates a GRanges object but the narrowPeak column names are missing.
 #' This function adds them in.
 #'
-#' @param x Granges object
+#' @param x GRanges object
 #'
-#' @return Granges object
+#' @return GRanges object
 #' @export
-add_narrowPeak_columnNames = function (x) {
+add_broadPeak_columnNames = function (x) {
   # Set metadata column names to reflect narrowPeaks format
   # see http://genome.ucsc.edu/FAQ/FAQformat.html#format12
   # and https://pypi.org/project/MACS2/ for more info
-  names(S4Vectors::mcols(x)) <- c("name", "score", "strand", "signalValue", "pValue", "qValue", "Position relative to peak start")
+  names(S4Vectors::mcols(x)) <- c("name", "score", "strand", "signalValue", "pValue", "qValue")
   return (x)
 
 }
-
