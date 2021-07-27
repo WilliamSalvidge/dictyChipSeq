d491b8bf32c24db547bc8ae2f281ee409c015da2
William Salvidge <williamsalvidge@Williams-MacBook-Air.local>
2021-07-27 10:13:24 +0100

    Added selectPromoterGRangesBasedOnTagMatrixIndex function and documentation. Modified getTagMatricesforDiffExpLevelBins and documentation. Added info about how to do all() call which could be helpful to compare if indices are the same between Windows GRanges objects.

---
+++ R/selectPromoterGRangesBasedOnTagMatrixIndex.R
@@ -0,0 +1,25 @@
+
+#' selectPromoterGRangesBasedOnTagMatrixIndex
+#'
+#' @param x Output from getTagMatricesForDiffExpLevelBins which will likely be a list of tagMatrices and Windows / Promoters GRanges with index column
+#'
+#' @return A new list. Each element is based on one of the incoming tagMatrices in the input list. Each element in the new list is also a list. These elements show
+#' the Promoter GRange object subsetted by the rownames of the tagMatrix. Where the rowname of the tagMatrix matches the index column in the Windows / Promoter GRange
+#' object, that row is returned. This is also done in a strand specific way to generate + and - strand Promoter GRange objects.
+#' @export
+selectPromoterGRangesBasedOnTagMatrixIndex = function(x) {
+
+  listOfIndices = list()
+
+  for (i in names(x)) {
+
+    listOfIndices[[i]] = list()
+    listOfIndices[[i]][["Indices"]] = x[[i]]$Windows[x[[i]]$Windows$index %in% row.names(x[[i]]$tagMatrix), ]
+    listOfIndices[[i]][["PlusIndices"]] = listOfIndices[[i]][["Indices"]][strand(listOfIndices[[i]][["Indices"]]) == "+", ]
+    listOfIndices[[i]][["NegIndices"]] = listOfIndices[[i]][["Indices"]][strand(listOfIndices[[i]][["Indices"]]) == "-", ]
+
+  }
+
+  return (listOfIndices)
+
+}
